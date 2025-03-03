8ded49a86d59785a85ff39d59e51f2d994b255bd
KandzaP <kandzapure@gmail.com>
2025-03-03 13:19:12 +0200

    invent

---
+++ view/inventory.php
@@ -0,0 +1,28 @@
+<?php include './components/head.php';?>
+<?php include './components/navbar.php';?>
+
+<h1 class="text-2xl">Dashboard</h1>
+<?php 
+/*
+product id
+product name
+category
+quantity
+price
+*/
+?>
+
+<table class="table table-compact border-2 border-white">
+    <thead>
+        <tr>
+            <th>Product ID</th>
+            <th>Product Name</th>
+            <th>Category</th>
+            <th>Quantity</th>
+            <th>Price</th>
+        </tr>
+    </thead>
+    <tbody>
+        
+    </tbody>
+</table>
