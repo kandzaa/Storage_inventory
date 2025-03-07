948fc7e5b5c72c60880311cec5e0d70367b3866d
Kevins Markuss Kanalis <KevinsKanalis@gmail.com>
2025-03-07 09:47:54 +0200

    Role validatione and login validatione works

--- Controller/DashboardController.php
+++ Controller/DashboardController.php
@@ -1,10 +1,15 @@
 <?php
 
+// require "Validator.php";
 class DashboardController
 {
     public function dashboard()
     {
+        if (Validator::Role() == false) { //only can use this if logged in
+            header("Location: /login");
+            exit();
+        }
+        var_dump($_SESSION);
         include './view/dashboard.php';
     }
-    
 }
