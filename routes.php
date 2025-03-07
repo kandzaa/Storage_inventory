a13890eacf944a0e4d73383c0e9c47856f1aee69
eaterz <muraskoruslans@gmail.com>
2025-03-07 08:44:26 +0200

    route

--- routes.php
+++ routes.php
@@ -2,7 +2,6 @@
 require "Controller/IndexController.php";
 require "Controller/AuthController.php";
 require "Controller/DashboardController.php";
-require "Controller/InventoryController.php";
 require "Router.php";
 
 
@@ -14,11 +13,9 @@ $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
 $router->get('/', [IndexController::class, 'index']);
 $router->get('/login', [AuthController::class, 'login']);
-$router->post('/login/process', [AuthController::class, 'processLogin']);
 $router->get('/register', [AuthController::class, 'register']);
-$router->post('/register/process', [AuthController::class, 'processRegistration']);
 $router->get('/dashboard', [DashboardController::class, 'dashboard']);
-$router->get('/inventory', [InventoryController::class, 'inventory']);
+$router->post('/register/process', [AuthController::class, 'processRegistration']);
 
 
 $router->route($url, $method);
