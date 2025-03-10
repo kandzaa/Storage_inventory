bc19e2d597233da56a46737502ffcee2b06e18c5
kandzaa <ipa22.s.purins@vtdt.edu.lv>
2025-03-10 08:44:22 +0200

    navbar fix

--- components/navbar.php
+++ components/navbar.php
@@ -2,13 +2,11 @@
 
 <div class="navbar bg-base-100">
   <div class="flex-1">
-  <a href="/login" class="btn btn-ghost mr-3">Login</a>
-  <a href="/register" class="btn btn-ghost mr-3">Register</a>
     <?php 
-    if (!Validator::Role() && $_SERVER['REQUEST_URI'] !== '/login') { 
-      header("Location: /login");
-      exit();
-    } else if (Validator::Role()) {
+    if (!Validator::Role()) { 
+      echo '<a href="/login" class="btn btn-ghost mr-3">Login</a>
+            <a href="/register" class="btn btn-ghost mr-3">Register</a>';
+    } else {
       echo '
       <a href="/dashboard" class="btn btn-ghost mr-3">Dashboard</a>
       <a href="/inventory" class="btn btn-ghost mr-3">Inventory</a>
